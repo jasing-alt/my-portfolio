187eab3d366739aa651cc66ed556df7b5ea4a0d7
Aisa <aisacardente55@gmail.com>
2026-08-31 14:36:48 +0800

    First React portfolio setup

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>I’m Jassy, a Computer Technology student at USJR and currently an intern at NCTV 360 in the web development department. I enjoy building web applications, designing user interfaces, and exploring ERP systems like SAP Logistics.</p>
+    </section>
+  );
+}
+
+export default About;
